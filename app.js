82e2ede0fa7e6b44492fd6a4915457f9e1658ea7
Hienb2014833 <hienb2014833@student.ctu.edu.vn>
2023-09-20 22:47:52 +0700

    Cai dat express chay thong bao xin chao

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const cors = require("cors");
-const req = require("express/lib/request");
+
 const contactsRouter = require("./app/routes/contact.route");
 const ApiError = require("./app/api-error");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/contacts", contactsRouter);
 
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to contact book application."});
+});
 app.use((req,res,next)=>{
     return next(new ApiError(404,"Resource not found"));
 });
@@ -16,11 +18,9 @@ app.use((req,res,next)=>{
 app.use((err,req,res,next)=>{
     return res.status(err.statusCode || 500).json({
         message: err.message || "Internal Server Error",
-});
-});
-
-app.get("/", (req, res) => {
-    res.json({ message: "Welcome to contact book application."});
+    });
 });
 
+
+app.use("/api/contacts", contactsRouter);
 module.exports = app;
